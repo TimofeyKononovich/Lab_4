4fde8b799004c76a2e6e5d5c4ef30fe0edefe42b
Timofey <timofeykononovich7@gmail.com>
2020-12-11 13:48:07 +0300

    First commit

---
+++ Lab_4.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.*;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.*;
+
+class Lab_4 {
+    public static void main(String[] args) {
+    }
+}
